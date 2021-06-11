72d2a9a6a5913d48508d2949b4222aa6e9d9b9d0
denys-u <denys.udovik@nure.ua>
2021-06-11 15:54:01 +0300

    created providers

---
+++ WebApplication1/WebApplication1/Providers/Abstractions/IGameProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.Entyties;
+
+namespace WebApplication1.Providers.Abstractions
+{
+    public interface IGameProvider
+    {
+        Task<string> CreateAsync(Game game);
+        Task<bool> UpdateAsync(string id);
+        Task<bool> DeleteAsync(Game game);
+        Task<Game> GetByIdAsync(string id);
+    }
+}
