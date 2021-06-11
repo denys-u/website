e3f07facf71ef7783779e0c28ef6cb9732710fb0
denys-u <denys.udovik@nure.ua>
2021-06-11 15:01:51 +0300

    created Entity Game

---
+++ WebApplication1/WebApplication1/Models/Entyties/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Entyties
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Developer { get; set; }
+
+        public DateTime Release { get; set; }
+
+    }
+}
